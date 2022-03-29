2d41ebb530ce61bde45bca8b0f47adb90a8bf1f6
raikkonen2008 <102561993+raikkonen2008@users.noreply.github.com>
2022-03-28 21:18:32 -0300

    primeiro commit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,25 @@
-
+var box ;
 function setup() {
   createCanvas(400,400);
+  box = createSprite (200,200,30,30)
 }
 
 function draw() 
 {
   background(30);
-
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.x=box.x+1
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.x=box.x-1
+  }
+  if (keyIsDown(UP_ARROW)) {
+box.y=box.y-1
+   }
+   if (keyIsDown(DOWN_ARROW)) {
+     box.y=box.y+1
+   }
+drawSprites ()
 }
 
 
